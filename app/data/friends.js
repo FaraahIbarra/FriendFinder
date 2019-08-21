e7ec228b3d221cc14593a9577f003cd03b4604c9
Faraah <ibarra.faraah@gmail.com>
2019-08-21 18:05:55 -0500

    finished app

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendsData = [
+    {
+        name: "Michelle Obama",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Michelle_Obama_2013_official_portrait.jpg",
+        scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+        name: "Vince Vaughn",
+        photo: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2016/04/gettyimages-477782050.jpg",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: "Angelina Jolie",
+        photo: "https://www.hellomagazine.com/imagenes/film/2019073075915/angelina-jolie-shocks-with-announcement-about-career/0-368-501/angelina-jolie-shocking-news-t.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Dwayne Johnson",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/440px-Dwayne_Johnson_2%2C_2013.jpg",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Steve Harvey",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/SteveHarveyHWOFMay2013_%28cropped%29.jpg/440px-SteveHarveyHWOFMay2013_%28cropped%29.jpg",
+        scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Taylor Swift",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Taylor_Swift_2_-_2019_by_Glenn_Francis_%28cropped%29.jpg/440px-Taylor_Swift_2_-_2019_by_Glenn_Francis_%28cropped%29.jpg",
+        scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+        name: "Elon Musk",
+        photo: "https://amp.businessinsider.com/images/59f8dc483e9d25db458b5dfc-750-563.jpg",
+        scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+];
+
+module.exports = friendsData;
