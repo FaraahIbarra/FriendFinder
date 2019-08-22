e087cdeb8ff4f174a5152ceabf3985de9c21fdcd
Faraah <ibarra.faraah@gmail.com>
2019-08-21 19:06:29 -0500

    apiRoutes changed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var apiRoutes = require('./app/routing/api-routes.js');
-var htmlRoutes = require('./app/routing/html-routes.js');
+var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
